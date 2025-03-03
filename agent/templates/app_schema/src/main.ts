73b5980ac089756c75e7464b0a3232e4bc27d8ab
Arseni Kravchenko <arseni@neon.tech>
2025-03-03 14:51:31 +0100

    clean test

--- agent/templates/app_schema/src/main.ts
+++ agent/templates/app_schema/src/main.ts
@@ -3,15 +3,16 @@ import { launchHttpServer } from './http-server';
 import { launchTelegram } from './telegram';
 
 function start() {
- switch (env.RUN_MODE) {
-   case 'http-server':
+  switch (env.RUN_MODE) {
+    case 'http-server':
+      launchHttpServer();
+      break;
+    case 'telegram':
+      launchTelegram();
+      break;
+    default:
+      throw new Error(`invalid run mode: ${env.RUN_MODE}`);
+  }
+}
 
-
-     launchHttpServer();
-     break;
-   case 'telegram':
-     launchTelegram();
-     break;
-   default:
-     throw new Error('Invalid RUN_MODE');
- }
+start();
