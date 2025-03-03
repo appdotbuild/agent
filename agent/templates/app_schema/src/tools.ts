313fc910449e3d1c904eaa901fad9e138eb47a50
Eugene K <evgenii@neon.tech>
2025-03-03 12:08:07 +0000

    Add default web search to make bot useful

--- agent/templates/app_schema/src/tools.ts
+++ agent/templates/app_schema/src/tools.ts
@@ -1,6 +1,6 @@
 import { z } from 'zod';
 import * as greet from './handlers/dummy_handler';
-
+import * as web_search from './handlers/web_search';
 interface ToolHandler<argSchema extends z.ZodObject<any>> {
   name: string;
   description: string;
@@ -14,5 +14,11 @@ export const handlers = [
     description: 'create a greeting message',
     handler: greet.handle,
     inputSchema: greet.greetUserParamsSchema,
-  },
+    },
+    {
+        name: "web_search",
+        description: "search the web for information",
+        handler: web_search.handle,
+        inputSchema: web_search.webSearchParamsSchema,
+    }
 ] satisfies ToolHandler<any>[];
