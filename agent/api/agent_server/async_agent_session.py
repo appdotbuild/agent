682e2049f7c1072d26b66bc2a798bd4eeb427a4a
Eugenek <evgenii@neon.tech>
2025-04-17 16:21:13 +0100

    Update async_agent_session.py

--- agent/api/agent_server/async_agent_session.py
+++ agent/api/agent_server/async_agent_session.py
@@ -24,7 +24,7 @@ import warnings
 logger = logging.getLogger(__name__)
 
 
-// DEPRECATED: use agent/trpc_agent/agent_session.py instead!
+# DEPRECATED: use agent/trpc_agent/agent_session.py instead!
 class AsyncAgentSession(AgentInterface):
     def __init__(self, application_id: str | None= None, trace_id: str | None = None, settings: Optional[Dict[str, Any]] = None):
         """Initialize a new agent session"""
