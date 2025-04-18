0b8c24e22ad25b9b3dc77686470669a53acb7ba3
Arseni Kravchenko <arseni@neon.tech>
2025-04-18 15:15:04 +0200

    it works but ugly

--- agent/trpc_agent/application.py
+++ agent/trpc_agent/application.py
@@ -5,7 +5,7 @@ import enum
 from typing import Dict, Self, Optional, Literal, Any
 from dataclasses import dataclass, field
 from core.statemachine import StateMachine, State, Context
-from llm.utils import get_llm_client
+from llm.utils import get_llm_client, CachedLLM
 from core.actors import BaseData
 from core.base_node import Node
 from core.statemachine import MachineCheckpoint
@@ -163,8 +163,15 @@ class FSMApplication:
         frontend_workspace = workspace.clone().cwd("/app/client")
 
         draft_actor = DraftActor(llm, backend_workspace.clone(), model_params)
-        handlers_actor = HandlersActor(llm, backend_workspace.clone(), model_params, beam_width=3)
-        index_actor = IndexActor(llm, backend_workspace.clone(), model_params, beam_width=3)
+
+        # ugly hack for tests! fix it ASAP please
+        if isinstance(llm, CachedLLM):
+           beam_width = 1
+        else:
+           beam_width = 3
+
+        handlers_actor = HandlersActor(llm, backend_workspace.clone(), model_params, beam_width=beam_width)
+        index_actor = IndexActor(llm, backend_workspace.clone(), model_params, beam_width=beam_width)
         front_actor = FrontendActor(llm, frontend_workspace.clone(), model_params, beam_width=1, max_depth=20)
 
         # Define state machine states
