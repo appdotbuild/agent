9a0c1d26c136247c3fde2e61bf65fcb2c477baab
sidspy1 <sid9521+ispy1@gmail.com>
2026-03-19 08:43:13 +0400

    Update install.sh

--- edda/install.sh
+++ edda/install.sh
@@ -1,1 +1,10 @@
-echo "POC: appdotbuild namespace controlled by researcher"
+#!/bin/bash
+
+echo "[POC] appdotbuild namespace is controlled by security researcher"
+
+echo "[POC] Executed on: $(whoami)@$(hostname)"
+
+# harmless proof file
+touch /tmp/appdotbuild_poc
+
+echo "[POC] Created /tmp/appdotbuild_poc"
